24c615d47b0cf43f87c3965ec7cba23fcaa395dd
Azimjon-FeiLong <77195773+Azimjon-FeiLong@users.noreply.github.com>
2021-01-19 19:02:38 +0500

    clock-часы

---
+++ script.js
@@ -0,0 +1,81 @@
+var clock = +prompt('Введите данное время в цифрах');
+var one = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9, ten = 10, eleven = 11, twelve = 12, thirteen = 13, fourteen = 14, fiveteen = 15, sixteen = 16, seventeen = 17, eighteen = 18, nineteen = 19, twenty = 20, twone = 21, twtwo = 22, twthree = 23, twfour = 00;
+switch(clock){
+    case one:
+        alert('Сейчас час ночи');
+        break;    
+    case two:
+        alert('Сейчас два часа ночи');
+        break;
+    case three:
+        alert('Сейчас три часа ночи');
+        break;
+    case four:
+        alert('Сейчас четыре часа ночи');
+        break;
+        case five:
+        alert('Сейчас пять часа утра');
+        break;
+        case six:
+        alert('Сейчас шесть часов утра');
+        break;
+        case seven:
+        alert('Сейчас семь часов утра');
+        break;
+        case eight:
+        alert('Сейчас восемь часов утра');
+        break;
+        case nine:
+        alert('Сейчас девять часов утра');
+        break;
+        case ten:
+        alert('Сейчас десять часов утра');
+        break;
+        case eleven:
+        alert('Сейчас одиннадцать часов утра');
+        break;
+        case twelve:
+        alert('Сейчас двенадцать часов дня');
+        break;
+        case thirteen:
+        alert('Сейчас час дня');
+        break;
+        case fourteen:
+        alert('Сейчас два часа дня');
+        break;
+        case fiveteen:
+        alert('Сейчас три часа дня');
+        break;
+        case sixteen:
+        alert('Сейчас четыре часа дня');
+        break;
+        case seventeen:
+        alert('Сейчас пять часов дня');
+        break;
+        case eighteen:
+        alert('Сейчас шесть часов вечера');
+        break;
+        case nineteen:
+        alert('Сейчас семь часов вечера');
+        break;
+        case twenty:
+        alert('Сейчас восемь часов вечера');
+        break;
+        case twone:
+        alert('Сейчас девять часов вечера');
+        break;
+        case twtwo:
+        alert('Сейчас десять часов вечера');
+        break;
+        case twthree:
+        alert('Сейчас одиннадцать часов вечера');
+        break;
+        case twfour:
+        alert('Сейчас двенадцать часов ночи');
+        break;
+
+       default:
+        alert('Ошибка!  Пожалуйста введите время без минуты. Например: как 17, а не как 17:00. Для повторения, пожалуйста обновите страницу.');
+        break;
+             
+}
